00974e399864b9e5365e8ed0ac28f07beb00cb14
Angelica <Angelica137@users.noreply.github.com>
2021-05-23 22:57:22 +0100

    5 returns Plang

--- src/main/java/RainDrops.java
+++ src/main/java/RainDrops.java
@@ -2,6 +2,8 @@ public class RainDrops {
 	public String translate(int number) {
 		if (number % 3 == 0) {
 			return "Pling";
+		} else if (number % 5 == 0) {
+			return "Plang";
 		} else {
 			return Integer.toString(number);
 		}
