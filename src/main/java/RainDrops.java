900179f213ba939498227f45644e8de9eb4ba266
Angelica <Angelica137@users.noreply.github.com>
2021-05-23 23:07:02 +0100

    255 returns PlingPlang, green

--- src/main/java/RainDrops.java
+++ src/main/java/RainDrops.java
@@ -1,6 +1,8 @@
 public class RainDrops {
 	public String translate(int number) {
-		if (number % 3 == 0) {
+		if (number % 3 == 0 && number % 5 == 0) {
+			return "PlingPlang";
+		} else if (number % 3 == 0) {
 			return "Pling";
 		} else if (number % 5 == 0) {
 			return "Plang";
